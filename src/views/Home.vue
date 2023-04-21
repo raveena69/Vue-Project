760665bde256941a8c6c1e60135afae5493b47d0
Raveena Amarasiriwardena <58651579+raveena69@users.noreply.github.com>
2023-04-21 21:45:27 +0530

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <HelloWorld />
+  <div>
+    
+  </div>
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
---
+++ src/views/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- <HelloWorld /> -->
+  <div>
+    <!-- <h1>Hello World</h1> -->
+    
+    <v-container fluid class="bg-green-lighten-2">
+      <v-row>
+        <v-col cols="4">
+          <h1>Welcome to Plante Latest Website</h1>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script setup>
+  // import HelloWorld from '@/components/HelloWorld.vue'
+</script>
